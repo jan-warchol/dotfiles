f73615d346840d8b0a1c9d7c6c71c5db5a4d8321
Jan Warchoł <jan.warchol@gmail.com>
2015-02-23 22:10:41 +0100

    Adjust some aliases to my preferences and add new ones

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # redefine some commands by adding "default" settings
-alias ls='ls --color=auto'
+alias ls='ls --color=auto --group-directories-first'
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias rgrep='rgrep --color=auto'
-alias less='less --RAW-CONTROL-CHARS'  # parses color codes!
+alias less='less --RAW-CONTROL-CHARS --ignore-case'  # parses color codes!
 
 # color-forced grep (will color matches even when piped to less!)
 alias grp='grep --line-number --color=always'
@@ -16,7 +16,24 @@ alias egp='egrep --line-number --color=always'
 alias l='ls --file-type --ignore-backups'
 alias la='ls --almost-all --file-type'
 alias ll='ls -l --human-readable --almost-all --file-type'
-alias ld='ll --group-directories-first'
+alias lk='ls -l --human-readable --file-type'
+
+# tree
+alias tre2='tree -C -L 2 --dirsfirst --filelimit 50'
+alias tre3='tree -C -L 3 --dirsfirst --filelimit 30'
+alias tre4='tree -C -L 4 --dirsfirst --filelimit 20'
+alias tre5='tree -C -L 5 --dirsfirst --filelimit 15'
+alias trea2='tree -C -L 2 -a --dirsfirst --filelimit 80'
+alias trea3='tree -C -L 3 -a --dirsfirst --filelimit 50'
+
+# vagrant
+alias vt='vagrant'
+alias vts='vagrant status'
+alias vtsh='vagrant ssh'
+alias vtu='vagrant up'
+alias vtus='vagrant up && vagrant ssh'
+alias vtupo='vagrant up --provider=openstack'
+alias vtd='vagrant destroy -f'
 
 # Some commands are so common that they deserve one-letter shortcuts :)
 alias g='git'
@@ -28,6 +45,9 @@ alias L='less'  # typing |L is very convenient, especially using left shift
 
 # miscellaneous
 mdc() { mkdir --parents "$@"; cd "$@"; }
+alias op='nautilus $(pwd)'
+alias monl='xrandr --output HDMI1 --rotate left; xrandr --output DP1 --rotate left'
+alias monn='xrandr --output HDMI1 --rotate normal; xrandr --output DP1 --rotate normal'
 
 # Show a notification when a command finishes - use like this:  sleep 10; alert
 # Taken from Ubuntu's default .bashrc.
