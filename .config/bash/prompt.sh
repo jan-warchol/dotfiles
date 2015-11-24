7dc3bedff6ded5754c3ef26cd4c50fb6b06d46cd
Jan Warchoł <jan.warchol@gmail.com>
2015-11-24 22:44:02 +0100

    Prompt: always use space as separator

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -29,10 +29,8 @@ fi
 # selecting with a double click) and display username in a different color.
 if [ -n "$SSH_CONNECTION" ]; then
     usercolor="\[${MAGENTA}\]"
-    separator=":"
 else
     usercolor="\[${BLUE}\]"
-    separator=" "
 fi
 
 # Show notification when the shell was lauched from ranger
@@ -48,5 +46,5 @@ GIT_PS1_SHOWUNTRACKEDFILES=1
 GIT_PS1_DESCRIBE_STYLE="branch"
 GIT_PS1_SHOWUPSTREAM="verbose git"
 
-export PS1="${usercolor}\u@\h${pathcolor}${separator}\w${resetall}\$(__git_ps1)${ranger_notice}\n${smartdollar}"
+export PS1="${usercolor}\u@\h ${pathcolor}\w${resetall}\$(__git_ps1)${ranger_notice}\n${smartdollar}"
 export PS4="$(tput bold)>>> $(tput sgr0)"
