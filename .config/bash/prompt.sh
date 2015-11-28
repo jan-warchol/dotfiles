b320fd1ae14f3be4b354431de3c8b3bc0a8d7597
Jan Warchoł <jan.warchol@gmail.com>
2015-11-28 21:15:25 +0100

    Prompt: change color variable names

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -15,29 +15,29 @@ CYAN="\e[36m"
 WHITE="\e[37m"
 DEFAULT="\e[39m"
 
-RESETALL="\e[0m"
+RESET_COLOR="\e[0m"
 
 # Using \[ and \] around color codes in prompt is necessary to prevent strange issues!
-pathcolor="\[${CYAN}\]"
-resetall="\[${RESETALL}\]"
+PS1_PATH_COLOR="\[${CYAN}\]"
+PS1_RESET_COLOR="\[${RESET_COLOR}\]"
 
 if [ $EUID = 0 ]; then
-    usercolor="${RED}"
+    PS1_USER_COLOR="${RED}"
 fi
 
 # When I'm logged in via ssh, display the path in scp-like format (-> easy
 # selecting with a double click) and display username in a different color.
 if [ -n "$SSH_CONNECTION" ]; then
-    usercolor="${MAGENTA}"
+    PS1_USER_COLOR="${MAGENTA}"
 else
-    usercolor="${BLUE}"
+    PS1_USER_COLOR="${BLUE}"
 fi
 
 # Show notification when the shell was lauched from ranger
-rangercolor="\[${BLUE}\]"
-[ -n "$RANGER_LEVEL" ] && ranger_notice=" ${rangercolor}(in ranger)${resetall}"
+PS1_RANGER_COLOR="\[${BLUE}\]"
+[ -n "$RANGER_LEVEL" ] && ranger_notice=" ${PS1_RANGER_COLOR}(in ranger)${PS1_RESET_COLOR}"
 
-smartdollar="\[${BOLD}\]\\$ \[${RESETALL}\]"
+smartdollar="\[${BOLD}\]\\$ \[${RESET_COLOR}\]"
 
 # $(__git_ps1) displays git repository status in the prompt, which is extremely handy.
 # Read more: https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
@@ -46,5 +46,5 @@ GIT_PS1_SHOWUNTRACKEDFILES=1
 GIT_PS1_DESCRIBE_STYLE="branch"
 GIT_PS1_SHOWUPSTREAM="verbose git"
 
-export PS1="\$(echo -e \${usercolor}\u@\h) ${pathcolor}\w${resetall}\$(__git_ps1)${ranger_notice}\n${smartdollar}"
+export PS1="\$(echo -e \${PS1_USER_COLOR}\u@\h) ${PS1_PATH_COLOR}\w${PS1_RESET_COLOR}\$(__git_ps1)${ranger_notice}\n${smartdollar}"
 export PS4="$(tput bold)>>> $(tput sgr0)"
