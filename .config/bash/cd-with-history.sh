ab920895618f8e59a0261e8ac99d0901c5f07b69
Jan Warchoł <jan.warchol@gmail.com>
2015-12-07 09:18:08 +0100

    Bash: use `cd_with_history` name rather than `cd`
    This is much more descriptive, and it shouldn't matter that the name is
    long because it will be aliased anyway

--- .config/bash/cd-with-history.sh
+++ .config/bash/cd-with-history.sh
@@ -7,16 +7,16 @@
 #
 # Original version found at https://gist.github.com/mbadran/130469
 
-function _cd {
-    # typing just `_cd` will take you $HOME ;)
+function cd_with_history {
+    # typing just `cd` will take you $HOME ;)
     if [ "$1" == "" ]; then
         pushd "$HOME" > /dev/null
 
-    # use `_cd -` to visit previous directory
+    # use `cd -` to visit previous directory
     elif [ "$1" == "-" ]; then
         pushd $OLDPWD > /dev/null
 
-    # use `_cd -n` to go n directories back in history
+    # use `cd -n` to go n directories back in history
     elif [[ "$1" =~ ^-[0-9]+$ ]]; then
         for i in `seq 1 ${1/-/}`; do
             popd > /dev/null
@@ -26,7 +26,7 @@ function _cd {
     elif [ "$1" == "." ]; then
         pushd "$(pwd -P)" > /dev/null
 
-    # use `_cd -- <path>` if your path begins with a dash
+    # use `cd -- <path>` if your path begins with a dash
     elif [ "$1" == "--" ]; then
         shift
         pushd -- "$@" > /dev/null
@@ -48,6 +48,6 @@ function ld {
 }
 
 # replace standard `cd` with enhanced version, ensure tab-completion works
-alias cd=_cd
+alias cd=cd_with_history
 complete -d cd
 
