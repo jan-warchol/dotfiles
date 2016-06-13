64654d9b67694915ea94541bc3a4a7e5f30fbf4e
Jan Warchoł <jan.warchol@gmail.com>
2016-06-12 19:44:13 -0700

    Dircolors: fix for selenized

--- .config/bash/00-settings.sh
+++ .config/bash/00-settings.sh
@@ -32,6 +32,9 @@ export EDITOR="vim"
 
 export _FASD_DATA="$HOME/janek/fasd"
 
+# fix dircolors for selenized
+export LS_COLORS="$LS_COLORS:ow=1;7;34:st=30;44:su=30;41"
+
 # disable terminal flow control key binding, so that ^S will search history forward
 stty -ixon
 
