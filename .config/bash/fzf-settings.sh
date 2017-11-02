abb6eb29437cdba363b533f7c2a6fcc66f553a65
Jan Warchoł <jan.warchol@gmail.com>
2017-11-02 17:43:22 +0100

    FZF: ignore permission denied in /etc

--- .config/bash/fzf-settings.sh
+++ .config/bash/fzf-settings.sh
@@ -21,7 +21,7 @@ export FZF_ALT_C_OPTS="--preview 'tree -C -L 2 --dirsfirst {} | head -200'"
 bind -x '"\C-o\C-n": fzf-file-widget'
 bind -x '"\C-o\C-a": FZF_CTRL_T_COMMAND="find" fzf-file-widget'
 bind -x '"\C-o\C-h": FZF_CTRL_T_COMMAND="find ~" fzf-file-widget'
-bind -x '"\C-o\C-e": FZF_CTRL_T_COMMAND="find /etc" fzf-file-widget'
+bind -x '"\C-o\C-e": FZF_CTRL_T_COMMAND="find /etc 2>/dev/null" fzf-file-widget'
 bind -x '"\C-o\C-g": FZF_CTRL_T_COMMAND="git ls-files" fzf-file-widget'
 bind -x '"\C-o\C-d": FZF_CTRL_T_COMMAND="GIT_DIR=~/.dotfiles.git git ls-files; find -L ~/.ssh; find ~/.fzf/shell" fzf-file-widget'
 bind -x '"\C-o\C-i": FZF_CTRL_T_COMMAND="fasd -Rl" FZF_DEFAULT_OPTS="--no-sort --bind ctrl-s:toggle-sort" fzf-file-widget'
