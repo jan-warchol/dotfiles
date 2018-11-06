3f5505309e38d3513c2ad0fd5d2f0d4f1d378fca
Jan Warchoł <jan.warchol@gmail.com>
2018-11-06 17:57:54 +0100

    Rewrite function for finding multifile folders
    This should make it clearer to navigate the output

--- bin/find-multifile-dirs.sh
+++ bin/find-multifile-dirs.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-find "$1" -maxdepth 2 -type d -print0 |
+helper() {
+  find "$1" -mindepth 1 -maxdepth $2 -type d -print0 |
   while IFS= read -d '' dir; do
-    echo "$(find "$dir" -print0 | grep -zc .) $dir"
+    count=$(find "$dir" -print0 | grep -zc .)
+    if (( $count > 20 )); then
+      printf "        %-6s  %s\n" $count $dir
+    fi
   done |
   sort -rn |
-  head -100
+  head -20
+}
+
+while read size dir; do
+  printf "%-6s  %s\n" $size $dir
+  while read s d; do
+    printf "    %-6s  %s\n" $s $d
+    helper $d 2"    "
+  done < <(helper $dir 1)
+  echo
+done < <(helper . 1)
