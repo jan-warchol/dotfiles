647c2d68dda571509ab94506f3b2ea3a455b9447
Jan Warchoł <jan.warchol@gmail.com>
2019-03-21 20:10:07 +0100

    Workshops: re-enable rm, mv and cp
    Normally I use "safe" versions of these (and I'm pretty used to the names of
    the safe versions), but to avoid confusion I disable them and oveload default
    names.

--- .config/bash/safeguards.sh
+++ .config/bash/safeguards.sh
@@ -4,13 +4,8 @@
 
 # Instead of rm, I use trash-cli (https://github.com/andreafrancia/trash-cli)
 # which moves files to system trash.  It can be installed with pip or apt-get.
-alias tp='trash-put --verbose'
 
-alias mw='\mv --interactive --verbose'
-alias kp='\cp --recursive --backup=numbered --verbose'
-
-# Make sure that I won't accidentally use plain rm, mv or cp
-alias rm='echo "This is not the command you are looking for."; false'
-alias mv='echo "This is not the command you are looking for."; false'
-alias cp='echo "This is not the command you are looking for."; false'
+alias rm='trash-put'
+alias mv='\mv --interactive --verbose'
+alias cp='\cp --recursive --backup=numbered'
 # Bypass these aliases by prepending a slash, e.g. \rm file-without-hope
