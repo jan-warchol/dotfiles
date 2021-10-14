d4c8b4a7748cc02b4c955069e9998f83e9c997b1
Jan Warchoł <jan.warchol@gmail.com>
2021-10-14 16:38:07 +0200

    Let both shifts trigger caps lock

--- .config/bash/general-settings.sh
+++ .config/bash/general-settings.sh
@@ -2,6 +2,8 @@
 # This file must be loaded first, because some other settings depend on it
 # (for example some aliases can be defined only after fasd initialization).
 
+setxkbmap -option shift:both_capslock
+
 # enable autocompletion
 if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     . /etc/bash_completion
