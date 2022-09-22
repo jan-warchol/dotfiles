01517cdb04bd1bbda0ccdad0c87fb5d83c5ef392
Jan Warchoł <jan.warchol@gmail.com>
2022-09-22 21:06:16 +0200

    History: binding for removing last command from history
    Quite often I make a typo and don't want to keep wrong command in the
    history. What's nice about this alias is that the command stays in the
    session history until next prompt, so I can recall it easily for
    editing.

--- .config/bash/history/settings.sh
+++ .config/bash/history/settings.sh
@@ -30,3 +30,14 @@ if [[ -e $HISTBACKUP && \
     \cp $HISTMERGED $HISTBACKUP
 fi
 
+__remove_last_history_entry() {
+    [[ ! -e "$HISTFILE.$$" ]] && return 1
+    tail -2 "$HISTFILE.$$" >> "$HISTFILE.removed"
+    echo "Command '$(tail -1 "$HISTFILE.$$")' removed from history."
+    head -n -2 "$HISTFILE.$$" > "$HISTFILE.tmp"
+    \mv "$HISTFILE.tmp" "$HISTFILE.$$"
+}
+
+bind -x '"\C-x\C-r": __remove_last_history_entry'
+bind '"\C-x\C-t": "\C-x\C-r"'
+bind '"\C-x\C-v": "\C-x\C-r\C-p"'
