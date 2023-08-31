52a2ae039eaa561e2a10ba98b6645124dd20b0fe
Jan Warchoł <jan.warchol@gmail.com>
2023-08-31 07:31:06 +0200

    WIP z travelynu

--- .bashrc
+++ .bashrc
@@ -32,3 +32,7 @@ if [ -e '/snap/google-cloud-cli/' ]; then
   source '/snap/google-cloud-cli/current/completion.bash.inc';
 fi
 # END ANSIBLE MANAGED BLOCK
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- .config/bash/fzf-settings.sh
+++ .config/bash/fzf-settings.sh
@@ -80,12 +80,49 @@ _fzf_kubectl() {
     tail +2 |
     fzf \
       --reverse \
+      --height 60% \
       --nth=1 \
-      --header='ctrl-L: stream logs,  ctrl-D: describe pod' \
+      --header='ctrl-L: stream logs,  ctrl-D: describe pod, ctrl-K: kill' \
       --bind="enter:abort+execute(echo {1})" \
       --bind="ctrl-l:abort+execute(echo kubectl logs -f {1})" \
-      --bind="ctrl-d:abort+execute(echo kubectl describe pod {1})"
+      --bind="ctrl-d:abort+execute(echo kubectl describe pod {1})" \
+      --bind="ctrl-k:abort+execute(echo kubectl delete pods {1} --grace-period=0 --force)"
 }
+
+bind '"\C-o\C-k": "$(_fzf_kubectl)\e\C-e\er"'
+
+# bindings for git - see functions defined in fzf-git-functions.sh
+bind '"\er": redraw-current-line'
+bind '"\C-g\C-f": "$(g_file)\e\C-e\er"'
+bind '"\C-g\C-b": "$(g_branch)\e\C-e\er"'
+bind '"\C-g\C-h": "$(g_hash)\e\C-e\er"'
+bind '"\C-g\C-s": "$(g_stash)\e\C-e\er"'
+bind '"\C-g\C-t": "$(fzf_git_tag)\e\C-e\er"'
+bind '"\C-g\C-g": "__fzf_git_checkout__\n"'
+
+__fzf_history_search() {
+  # get commands ran since last prompt in this session
+  update_history
+
+  # See https://github.com/junegunn/fzf/blob/master/shell/key-bindings.bash
+  local output opts script
+  opts="--height 50% $FZF_DEFAULT_OPTS --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS +m --read0"
+  script='BEGIN { getc; $/ = "\n\t"; $HISTCOUNT = $ENV{last_hist} + 1 } s/^[ *]//; print . "\t$_" if !$seen{$_}++'
+  output=$(
+    builtin fc -lnr -2147483648 |
+      last_hist=$(HISTTIMEFORMAT='' builtin history 1) perl -n -l0 -e "$script" |
+      FZF_DEFAULT_OPTS="$opts" fzf --query "$READLINE_LINE"
+  ) || return
+  READLINE_LINE=${output#*$'\t'}
+  if [[ -z "$READLINE_POINT" ]]; then
+    echo "$READLINE_LINE"
+  else
+    READLINE_POINT=0x7fffffff
+  fi
+}
+
+# replace default Ctrl-R mapping
+bind -x '"\C-r": __fzf_history_search'
 
 bind '"\C-o\C-k": "$(_fzf_kubectl)\e\C-e\er"'
 
